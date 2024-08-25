68cc8654a1363d0cce475f2db8458abda5dbbac0
AdministratorX2p <anirbanuchiha76@gmail.com>
2024-08-25 13:03:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID=1619182628
-    API_HASH="ksjs"
-    TOKEN="jaksks"
+    API_ID=23716755
+    API_HASH="5503b68991d161b6863dba06ff28f"
+    TOKEN="7517031277:AAGDXjbA7YYPgbd3uQSfDBpIH90Cf0cvdwM"
     SUDO = list(int(i) for i in os.environ.get("SUDO", "5264285143").split(" "))
-    START_IMG="jdjsjw"
-    BOT_ID=1013
-    BOT_USERNAME="djsjw"
+    START_IMG="Chcu6vhvProvhobot"
+    BOT_ID=7517031277
+    BOT_USERNAME="Chcu6vhvProvhobot"
     BOT_NAME="BAN ALL"
